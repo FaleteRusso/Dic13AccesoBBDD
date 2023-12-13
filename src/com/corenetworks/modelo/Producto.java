3ba796417c34ad23ffa65533cc00d55b9e90a080
FaleteRusso <faleterusso89@gmail.com>
2023-12-13 19:08:47 +0100

    Initial commit

--- src/com/corenetworks/modelo/Producto.java
+++ src/com/corenetworks/modelo/Producto.java
@@ -1,2 +1,75 @@
-package com.corenetworks.modelo;public class Productos {
+package com.corenetworks.modelo;
+
+import java.util.Objects;
+
+public class Producto {
+    private int IdProducto;
+    private String nombreProducto;
+    private double precioUnid;
+    private  int unidadesStock;
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "IdProducto=" + IdProducto +
+                ", nombreProducto='" + nombreProducto + '\'' +
+                ", precioUnid=" + precioUnid +
+                ", unidadesStock=" + unidadesStock +
+                '\n';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return IdProducto == producto.IdProducto && Double.compare(precioUnid, producto.precioUnid) == 0 && unidadesStock == producto.unidadesStock && Objects.equals(nombreProducto, producto.nombreProducto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(IdProducto, nombreProducto, precioUnid, unidadesStock);
+    }
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nombreProducto, double precioUnid, int unidadesStock) {
+        IdProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.precioUnid = precioUnid;
+        this.unidadesStock = unidadesStock;
+    }
+
+    public int getIdProducto() {
+        return IdProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        IdProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public double getPrecioUnid() {
+        return precioUnid;
+    }
+
+    public void setPrecioUnid(double precioUnid) {
+        this.precioUnid = precioUnid;
+    }
+
+    public int getUnidadesStock() {
+        return unidadesStock;
+    }
+
+    public void setUnidadesStock(int unidadesStock) {
+        this.unidadesStock = unidadesStock;
+    }
 }
--- src/com/corenetworks/presentacion/Main.java
+++ src/com/corenetworks/presentacion/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+package com.corenetworks.presentacion;
+
+import com.corenetworks.persistencia.AccesoProductoTabla;
+import com.corenetworks.persistencia.Conexion;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        Conexion c1 =new Conexion();
+        try {
+            c1.abrirConexion();
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        AccesoProductoTabla ap1= new AccesoProductoTabla();
+//        try {
+//            System.out.println(ap1.obtenerUno(5));
+//        } catch (SQLException | ClassNotFoundException e) {
+//            System.out.println(e.toString());
+//        }
+        try {
+            System.out.println(ap1.obtenerTodos());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.toString());
+        }
+    }
+
 }
